325511d218591e401f8ab6e9559eee9cf677c161
max2468tw <max2468tw.eecs04@g2.nctu.edu.tw>
2016-06-24 03:46:12 +0800

    Lab03 Test4

--- lab03/TriangleDrawer2.java
+++ lab03/TriangleDrawer2.java
@@ -4,7 +4,7 @@
 public class TriangleDrawer2 {
     public static void main(String[] args) {
         System.out.println();
-        for (int row = 0,SIZE = 10;row < SIZE;row++;) {
+        for (int row = 0,SIZE = 10;row < SIZE;row++) {
             for (int col = 0;col < row;col++) {
                 System.out.print ('*');
             }
