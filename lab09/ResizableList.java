01784002cf84b2b0bbcc73f69191176dd6bc54b0
max2468tw <max2468tw.eecs04@g2.nctu.edu.tw>
2016-07-07 17:59:35 -0700

    Lab09

--- lab09/ResizableList.java
+++ lab09/ResizableList.java
@@ -19,11 +19,13 @@ public class ResizableList extends FixedSizeList {
                 values[p] = val[p];
             }
         }
-        for (int p = i + 1; p <= count; p++) {
-            values[p] = values[p-1];
+        else{
+            for (int p = count; p > i; p--) {
+                values[p] = values[p-1];
+            }
+            values[i] = k;
+            count++;
         }
-        values[i] = k;
-        count++;
     }
     public void add(int k) {
         // YOUR CODE HERE
@@ -37,12 +39,9 @@ public class ResizableList extends FixedSizeList {
                 values[i] = val[i];
             }
         }
-        for(int i = 0; i < values.length; i++){
-            if(values[i] == 0){
-                values[i] = k;
-                break;
-            }
+        else {
+            values[count] = k;
+            count++;
         }
-        count ++;
     }
 }
