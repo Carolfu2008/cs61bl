b1139ade1fdd0460e4e425c02ed06954f1238870
alanyao <alanyao@berkeley.edu>
2016-08-08 00:50:06 -0700

    releasing lab24

---
+++ lab24/RegexPuzzles.java
@@ -0,0 +1,72 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPuzzles {
+    public static List<String> urlRegex(String[] urls) {
+        /* Your code here */
+        return null;
+    }
+
+    public static List<String> findStartupName(String[] names) {
+        /* Your code here */
+        return null;
+    }
+
+    public static BufferedImage imageRegex(String filename, int width, int height) {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("No such file found: " + filename);
+        }
+
+        // Possible initialization code
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Code for processing each line
+            }
+        } catch (IOException e) {
+            System.err.printf("Input error: %s%n", e.getMessage());
+            System.exit(1);
+        }
+
+        return null;
+    }
+
+    public static BufferedImage arrayToBufferedImage(int[][][] arr) {
+        BufferedImage img = new BufferedImage(arr.length, arr[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                int pixel = 0;
+                for (int k = 0; k < 3; k++) {
+                    pixel += arr[i][j][k] << (16 - 8*k);
+                }
+                img.setRGB(i, j, pixel);
+            }
+        }
+
+        return img;
+    }
+
+    public static void main(String[] args) {
+        /* For testing image regex */
+        BufferedImage img = imageRegex("mystery.txt", 400, 400);
+
+        File outputfile = new File("output_img.jpg");
+        try {
+            ImageIO.write(img, "jpg", outputfile);
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
+}
